2151f63202b91c8aeca7d5c401fe2d9b65bcf81d
Blake Varbai-Heward <Blake-Heward@hotmail.com>
2022-11-22 22:50:25 +1030

    Cards need to be populated with content next

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+// Add new book
+let counter = 0
+let myLibrary = []
+let bookCount = document.querySelector(".bookCount")
+const mainDiv = document.querySelector(".main")
+const newBook = document.querySelector(".newBook")
+const removeBook = document.querySelector(".removeBook")
+const toggleDiv = document.querySelector(".toggleDiv")
+
+const newH3 = document.createElement("h3") // Title
+const newH4 = document.createElement("h4") // Author
+const newInput = document.createElement("input") // Pages
+const newInputRead = document.createElement("input") // Read
+const newButton = document.createElement("button") // Delete Button
+
+// Add new book
+newBook.addEventListener("click", function() {
+  const newDiv = document.createElement("div")
+  mainDiv.appendChild(newDiv)
+  newDiv.classList.add("cards")
+  
+  // Calls function that adds user input into the card
+  newDiv.appendChild(newH3)
+  newDiv.appendChild(newH4)
+  newDiv.appendChild(newInput)
+  newDiv.appendChild(newInputRead)
+  newDiv.appendChild(newButton).classList.add("removeBook")
+  
+})
+
+// Adds to counter of books in library
+newBook.addEventListener("click", function() {
+  counter++
+  updateCount()
+})
+
+// Removes book
+removeBook.addEventListener("click", function() {
+  removeBook.parentElement.remove()
+  counter--
+  updateCount()
+})
+
+// Updates counter
+function updateCount() {
+  bookCount.innerHTML = counter
+}
+
 function Book(title, author, pages, read) {
   this.title = title
   this.author = author
@@ -15,13 +63,17 @@ function booksInLibrary(library) {
   }
 }
 
-let myLibrary = []
-const harryPotter = new Book("Harry Potter and the Order of the Phoenix", "J.K. Rowling", "576", true)
-const bareFootKids = new Book("BareFoot Kids", "Scott Pape", "224", true)
-const READMEtxt =  new Book("README.txt", "Chelsea Manning", "224", false)
-const safar = new Book("Safar", "Sarah Malik", "168", false)
 
-addBookToLibrary(harryPotter)
-addBookToLibrary(bareFootKids)
-addBookToLibrary(READMEtxt)
-addBookToLibrary(safar)
+
+
+
+
+// const harryPotter = new Book("Harry Potter and the Order of the Phoenix", "J.K. Rowling", "576", true)
+// const bareFootKids = new Book("BareFoot Kids", "Scott Pape", "224", true)
+// const READMEtxt =  new Book("README.txt", "Chelsea Manning", "224", false)
+// const safar = new Book("Safar", "Sarah Malik", "168", false)
+
+// addBookToLibrary(harryPotter)
+// addBookToLibrary(bareFootKids)
+// addBookToLibrary(READMEtxt)
+// addBookToLibrary(safar)
